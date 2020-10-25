8828da6611a9ed7a9b1da20a768d9452d1966da5
jp-loran <61357854+jp-loran@users.noreply.github.com>
2020-10-25 15:24:51 -0600

    Add files via upload

---
+++ tablaSimbolos.h
@@ -0,0 +1,100 @@
+/*En este archivo se definen las funciones relacionadas a la Tabla de Simbolos*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*La tabla se símbolos se manejará como una lista ligada
+cada nodo de la lista es una estructura con los campos requeridos*/
+typedef struct tablaSimbolos{
+    int pos;
+    char* nombre;
+    int tipo;
+    struct tablaSimbolos* siguienteRegistro;
+}tablaSimbolos;
+
+typedef struct{
+    struct tablaSimbolos* primerRegistro;
+}listaSimbolos;
+
+/*Con esta función inicializamos nuestra tabla de símbolos*/
+listaSimbolos crear_tabla(){
+    listaSimbolos lista;
+    lista.primerRegistro=NULL;
+    return lista;
+}
+
+/*Esta función nos permite mostrar el contenido de nuestra lista ligada o tabla de símbolos
+ dentro del archivo de salida que se le pase como parámetro*/
+void mostrar_tabla_simbolos(FILE *archivo,listaSimbolos lista){
+    if(lista.primerRegistro==NULL)
+        fprintf(archivo,"Tabla de simbolos vacia");
+    else{
+        tablaSimbolos *actual=lista.primerRegistro;
+        fprintf(archivo," ____________________\n");
+	    fprintf(archivo,"|Posicion|Nombre|Tipo| \n");        
+        while(actual !=0){
+            fprintf(archivo,"  %d \t   %s   %d",actual->pos,actual->nombre,actual->tipo);
+            actual=actual->siguienteRegistro;
+        }
+    }
+}
+
+/*Esta función agrega los registros a la tabla de símbolos, haciendo uso de memoria 
+dinámica y validando la existencia de los nombres*/
+void agregar_registro_simbolo(listaSimbolos *lista, char* nombre, int tipo){
+    if(buscar_registro_simbolo(lista,nombre)==-1){
+        if (lista->primerRegistro==NULL)
+        {
+            tablaSimbolos *registro = (tablaSimbolos*)malloc(sizeof(tablaSimbolos));
+            registro->pos=0;
+            registro->nombre=nombre;
+            registro->tipo=tipo;
+            registro->siguienteRegistro=NULL;
+            lista->primerRegistro=registro;
+        }
+        else
+        {
+            tablaSimbolos *actual = lista->primerRegistro;
+            int posicion;
+            while(actual->siguienteRegistro != 0){
+                actual=actual->siguienteRegistro;
+                posicion++;
+            }
+
+            tablaSimbolos *nuevoRegistro;
+            nuevoRegistro=(tablaSimbolos*)malloc(sizeof(tablaSimbolos));
+            nuevoRegistro->pos=posicion;
+            nuevoRegistro->nombre=nombre;
+            nuevoRegistro->tipo=tipo;
+            nuevoRegistro->siguienteRegistro=NULL;
+            actual->siguienteRegistro=nuevoRegistro;
+        }
+    }
+}
+
+/*Esta función permite buscar un registro por su nombre
+Devuelve un -1 cuando la lista se encuentra vacía o cuando no hay coincidencia
+De lo contrario devuelve un 0*/
+int buscar_registro_simbolo(listaSimbolos *lista, char* nombre){
+    if(lista->primerRegistro==NULL){
+        return -1;
+    }
+    else
+    {
+        tablaSimbolos *actual = lista->primerRegistro;
+        if(!strcmp(actual->nombre, nombre)){
+            return 0;
+        }
+        int posicion=0;
+        while (actual->siguienteRegistro != 0)
+        {
+            actual=actual->siguienteRegistro;
+            posicion++;
+            if(!strcmp(actual->nombre, nombre)){
+            return posicion;
+            }
+        }
+        return -1;
+    }
+    
+}
---
+++ tokens.h
@@ -0,0 +1,70 @@
+/*En este archivo se encuentran las funciones relacionadas a la creación de los tokens*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Los tokens se tratarán de la misma forma que la tabla de símbolos, es decir,
+mediante una lista ligada donde cada nodo es una estructura con los datos del token*/
+typedef struct Token
+{
+    int clase;
+    int posicion;
+    struct Token* siguienteToken;
+}token;
+
+typedef struct 
+{
+    struct Token* primerToken;
+}listaTokens;
+
+/*Función para incializar la lista*/
+listaTokens crear_lista_tokens(){
+    listaTokens lista;
+    lista.primerToken=NULL;
+    return lista;
+}
+
+/*Función para mostrar los tokens en un archivo*/
+void mostrar_lista_tokens(FILE *archivo, listaTokens lista){
+    if (lista.primerToken==NULL){
+        fprintf(archivo, "Lista de tokens vacía");
+    }
+    else{
+        token *actual= lista.primerToken;
+        while (actual != 0)
+        {
+            fprintf(archivo, "(%d,%d)", actual->clase,actual->posicion);
+            actual=actual->siguienteToken;
+        }
+         
+    }
+}
+
+/*Funcion que nos permite agregar tokens a la lista usando memoria dinámica*/
+void agregar_token(listaTokens *lista, int clase, int posicion){
+    if (lista->primerToken==NULL){
+        Token *token = (Token*)malloc(sizeof(Token));
+        token->clase=clase;
+        token->posicion=posicion;
+        token->siguienteToken=NULL;
+        lista->primerToken=token; 
+    }    
+    else
+    {
+        Token *actual= lista->primerToken;
+        while (actual->siguienteToken!=0)
+        {
+            actual=actual->siguienteToken;
+        }
+        Token *nuevoToken;
+        nuevoToken = (Token*)malloc(sizeof(Token));
+        nuevoToken->clase=clase;
+        nuevoToken->posicion=posicion;
+        nuevoToken->siguienteToken=NULL;
+        actual->siguienteToken=nuevoToken;
+    }
+    
+}
+
+
+
